452be556e7a48e8a5f6d38a642fa376948ecf68f
Julia Pfaffenzeller <julia.pfaffenzeller@web.de>
2024-01-24 15:59:38 +0100

    add new restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
 
   # A visitor can see the list of all restaurants.
   get "restaurants", to: "restaurants#index"
+
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
 end
